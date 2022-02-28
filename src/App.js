bc31c9112e40436cbc8b19c73e4f5efaa021b98d
Giorgi Shatirishvili <gio.shatira@gmail.com>
2022-02-28 18:58:56 +0400

    added some code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Greeting from './Greeting';
 import Welcome from './Welcome';
+import Top from './Top';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Top account={'Andukafar'} position={'CEO'} />
       <Welcome user="James Bond" age={50} />
       <Greeting title="Test Message" subTitle="subtitle" />
     </div>
---
+++ src/Top.js
@@ -0,0 +1,9 @@
+const Top = ({ account, position }) => {
+  return (
+    <div className="top">
+      Hello, {account}. You are currently logged in as {position}
+    </div>
+  );
+};
+
+export default Top;
