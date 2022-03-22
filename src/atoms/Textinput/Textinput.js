0d446fa979e9f93c5eb50e0799066a492743639c
Giorgi Shatirishvili <gio.shatira@gmail.com>
2022-03-23 00:56:17 +0400

    lecture 7.1

--- src/atoms/Textinput/Textinput.js
+++ src/atoms/Textinput/Textinput.js
@@ -1,15 +1,16 @@
-export const TextInput = ({ value, onChange, placeholder }) => {
+export const TextInput = ({ value, onChange, placeholder = '' }) => {
   const id = Math.random().toString();
   return (
     <div className="input-group mb-3">
       <span className="input-group-text" id={id}>
-        ❔
+        @
       </span>
       <input
         type="text"
         className="form-control"
         placeholder={placeholder}
         id={id}
+        name="filterTerm"
         value={value}
         onChange={onChange}
       />
